e66a8f0fae38510b0236002647b89f618d27a8b4
ammbra <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>
2021-11-04 11:06:59 +0100

    updated instructions

--- simulate-traffic-with-priority-agent.sh
+++ simulate-traffic-with-priority-agent.sh
@@ -1,12 +1,14 @@
-#kubectl apply -f activity/src/main/k8s/backend-benelux-v2.yml
-#
-#kubectl apply -f activity/src/main/k8s/backend-benelux.yml
-#
+#####comment these lines if you are not using OpenShift###
+
+kubectl apply -f activity/src/main/k8s/backend-benelux-v2.yml
+
+kubectl apply -f activity/src/main/k8s/backend-benelux.yml
+
 #####uncomment these lines if you are not using OpenShift####
-##kubectl apply -f <(istioctl kube-inject -f activity/src/main/k8s/backend-benelux-v2.yml)
-##kubectl apply -f <(istioctl kube-inject -f activity/src/main/k8s/backend-benelux.yml)
-#
-#kubectl apply -f activity/src/main/k8s/gateway.yaml
+#kubectl apply -f <(istioctl kube-inject -f activity/src/main/k8s/backend-benelux-v2.yml)
+#kubectl apply -f <(istioctl kube-inject -f activity/src/main/k8s/backend-benelux.yml)
+
+kubectl apply -f activity/src/main/k8s/gateway.yaml
 
 ###ONLY for OpenShift; comment the line below if working in a different environment
 export GATEWAY_URL=$(kubectl get route istio-ingressgateway -n istio-system -o jsonpath='{.status.ingress[0].host}')
@@ -14,10 +16,10 @@ export GATEWAY_URL=$(kubectl get route istio-ingressgateway -n istio-system -o j
 ###If you are not using OpenShift, please  uncomment the following line
 #export GATEWAY_URL=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].hostname}').
 
-#
-#kubectl delete virtualservice activity
-#kubectl delete destinationrule activity
-#
+
+kubectl delete virtualservice activity
+kubectl delete destinationrule activity
+
 kubectl apply -f activity/src/main/k8s/virtual-service-safari-activity-v2.yml
 kubectl apply -f activity/src/main/k8s/destination-rule-activity-v1-v2.yml
 
